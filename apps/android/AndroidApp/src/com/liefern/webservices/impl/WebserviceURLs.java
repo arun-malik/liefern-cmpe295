df8f787f0b315759a07dd639230865aa86d841e6
devmaven <malikarun86@gmail.com>
2015-04-26 01:05:47 -0700

    UI enhancement -change bg image -update card style list in requests, delivery, receipts, traveler and payments

--- apps/android/AndroidApp/src/com/liefern/webservices/impl/WebserviceURLs.java
+++ apps/android/AndroidApp/src/com/liefern/webservices/impl/WebserviceURLs.java
@@ -4,8 +4,6 @@ import java.net.URI;
 
 public interface WebserviceURLs {
 	String BASE_URL = "http://liefern.cloudapp.net:3000/api/";
-	//String BASE_URL = "http://10.0.0.14:3000/api/";
-//	String BASE_URL = "http://172.22.29.32:3000/api/";
 	String LOGIN_URL		= BASE_URL + "Liefernusers/login";
 	String SIGN_UP_URL		= BASE_URL + "Liefernusers";
 	String LOG_OUT_URL		= BASE_URL + "Liefernusers/logout";
