731386e42f98956eac4997c1b46e346288a2609f
devmaven <malikarun86@gmail.com>
2015-05-03 20:17:59 -0700

    created new Upsert api for geo location

--- node-rest/common/models/geo.js
+++ node-rest/common/models/geo.js
@@ -1,13 +1,72 @@
 module.exports = function(Geo) {
 
     Geo.beforeRemote('**', function(ctx, user, next) {
-
-        var header = ctx.req.headers;
-
-        if (!header.hasOwnProperty('token')){
-            next(new Error('please pass token in header'))
-        }
-
+        //
+        //var header = ctx.req.headers;
+        //
+        //if (!header.hasOwnProperty('token')){
+        //    next(new Error('please pass token in header'))
+        //}
+        //
         next();
     });
-};
+
+    Geo.remoteMethod(
+        'upsertGeo',
+        {
+            accepts: { arg: 'data', type: 'object', http: { source: 'body' } } ,
+            returns : {arg: 'response', type: 'object'},
+            http: {path: '/upsert', verb:'POST'},
+            description : 'Create record if doesnt exists or update same record'
+        }
+    );
+
+
+    Geo.upsertGeo = function(json,cb) {
+
+        if (!json.hasOwnProperty('userid')) {
+            cb(new Error('cannot find userid in input. Please logout and login again'), null);
+        }
+
+        Geo.findOne({  where:
+            {   userid :  json.userid
+
+            }
+            }, function(err,geoRec) {
+
+                if (err) {
+                    cb(new Error(err), null);
+                }
+
+                if (geoRec === null) {
+
+                    Geo.upsert(json, function (err, res) {
+                        if (err) {
+                            cb(new Error(err), null);
+                        }
+                        cb(null, res);
+                    });
+
+                } else {
+
+                    geoRec.lat = json.lat;
+                    geoRec.lng = json.lng;
+                    geoRec.radius = json.radius;
+
+                    Geo.upsert(geoRec, function (err, res) {
+                        if (err) {
+                            cb(new Error(err), null);
+                        }
+                        cb(null, res);
+                    });
+                }
+
+            }
+
+        );
+
+
+
+    }
+
+    };
